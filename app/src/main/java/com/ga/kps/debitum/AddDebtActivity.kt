7df6fbc99ac1fb24ccd84b60962f38375ef3b927
spartan1047 <c.eduardo.corona@hotmail.com>
2019-05-04 19:14:18 -0500

    Creado el list item para la deuda

--- app/src/main/java/com/ga/kps/debitum/AddDebtActivity.kt
+++ app/src/main/java/com/ga/kps/debitum/AddDebtActivity.kt
@@ -2,6 +2,10 @@ package com.ga.kps.debitum
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add_debt.*
 
 
@@ -14,6 +18,19 @@ class AddDebtActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         val ab = supportActionBar
         ab!!.setDisplayHomeAsUpEnabled(true)
-        title = "Hola"
+        title = getString(R.string.anadir_nueva_deuda)
+
+        tipoDeudaSP.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,this.resources.getStringArray(R.array.tipo_deuda))
+        tipoDeudaSP.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+               Toast.makeText(this@AddDebtActivity,"TeST", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 }
